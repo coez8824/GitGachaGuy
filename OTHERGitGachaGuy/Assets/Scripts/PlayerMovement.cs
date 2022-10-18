5906b5d4e1fe9a3b7b601c3189e30c8133120fc2
coez8824 <coez8824@my.uwstout.edu>
2022-10-18 10:50:35 -0500

    FUCK

--- OTHERGitGachaGuy/Assets/Scripts/PlayerMovement.cs
+++ OTHERGitGachaGuy/Assets/Scripts/PlayerMovement.cs
@@ -14,32 +14,41 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 lookVec;
     private Rigidbody2D rb;
 
+    private GunScript gunScript;
+
     // Start is called before the first frame update
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody2D>();
+
+        gunScript = GetComponent<GunScript>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        //Gets input for left "Move" stick
         inputMove = playerInput.actions["Move"].ReadValue<Vector2>();
-        moveVec = new Vector2(inputMove.x, inputMove.y);
+        moveVec = new Vector2(inputMove.x, inputMove.y); 
 
+        //Gets input for right "Look: stick
         inputLook = playerInput.actions["Look"].ReadValue<Vector2>();
-        //lookVec = new Vector2(inputLook.x, inputLook.y);
 
+        //Moves player based on input
         Vector2 currMovement = moveVec * playerSpeed * Time.deltaTime;
-
         rb.MovePosition(rb.position + currMovement);
 
+        //Activates if look stick is moved
         if (inputLook.magnitude > 0f)
         {
-            Vector3 currRotation = Vector3.left * inputLook.x + Vector3.up * inputLook.y;
+            Vector3 currRotation = Vector3.right * inputLook.x + Vector3.up * inputLook.y;
             Quaternion playerRotation = Quaternion.LookRotation(currRotation, Vector3.forward);
 
             rb.SetRotation(playerRotation);
+
+            if(gunScript.canShoot)
+                gunScript.shoot();
         }
 
     }
