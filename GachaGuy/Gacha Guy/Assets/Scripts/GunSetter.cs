22fa4c44ac11c2a409ff635b55edd9d01bd86dc7
coez8824 <coez8824@my.uwstout.edu>
2022-11-14 13:49:52 -0600

    UI For Sprint 4

--- GachaGuy/Gacha Guy/Assets/Scripts/GunSetter.cs
+++ GachaGuy/Gacha Guy/Assets/Scripts/GunSetter.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunSetter : MonoBehaviour
 {
     private GameManager gm;
 
     public GunScript GS;
+
+    public Image i;
+    public Sprite pistol;
+    public Sprite rifle;
+    public Sprite sniper;
 
     private void Start()
     {
@@ -21,14 +27,17 @@ public class GunSetter : MonoBehaviour
         {
             GS.setStats(1, 1, 7, .1f, n);
             GS.inf = true;
+            i.sprite = pistol;
         }
         else if (s == "Rifle")
         {
             GS.setStats(1, .1f, 25, .1f, n);
+            i.sprite = rifle;
         }
         else if (s == "Sniper")
         {
             GS.setStats(10, 2, 12, 0, n);
+            i.sprite = sniper;
         }
         else
         {
