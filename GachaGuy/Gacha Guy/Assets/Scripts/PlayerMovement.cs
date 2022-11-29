1bde868a3494b66ffbb9aeae3faf7c5fbf495fc6
coez8824 <coez8824@my.uwstout.edu>
2022-11-29 08:25:27 -0600

    New Controls
    Mouse and controller

--- GachaGuy/Gacha Guy/Assets/Scripts/PlayerMovement.cs
+++ GachaGuy/Gacha Guy/Assets/Scripts/PlayerMovement.cs
@@ -21,10 +21,22 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
 
     public bool mouse;
+    public bool controller;
 
     public Camera c;
 
     Vector2 move;
+
+    public GameObject touchUI;
+    public GameObject otherTouchUI;
+
+    private bool colorReloads;
+
+    //Vector2 newpos;
+    //Vector2 oldpos;
+    //Vector2 movement;
+
+    Vector2 rightstick;
 
     //Jacob's ice varible
     public bool ice;
@@ -40,7 +52,16 @@ public class PlayerMovement : MonoBehaviour
     {
         playerSpeed = gm.ps.SPD; //Player's speed = speed stat
 
-        if(!mouse)
+        //newpos = rb.position;
+        //movement = (newpos - oldpos);
+
+
+        //if (Vector2.Dot(rb.transform.forward, movement) < 0 )
+        //{
+        //    Debug.Log("back");
+        //}
+
+        if((!mouse)&&(!controller))
         {
             if (movementJoystick.joystickVec.y != 0) //If move joystick is moved
             {
@@ -64,12 +85,59 @@ public class PlayerMovement : MonoBehaviour
                     gm.gs.shoot();
             }
         }
-        else
+        else if(!controller)
         {
+            touchUI.SetActive(false);
+            otherTouchUI.SetActive(false);
+
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                gm.gs.reload();
+                colorReloads = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                gm.tossGun();
+            }
+
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                gm.swapGun();
+            }
+
+            if(colorReloads)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    gm.gs.reloadRed();
+                    colorReloads = false;
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                    gm.gs.reloadBlue();
+                    colorReloads = false;
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha3))
+                {
+                    gm.gs.reloadPink();
+                    colorReloads = false;
+                }
+            }
+
             move.x = Input.GetAxisRaw("Horizontal");
             move.y = Input.GetAxisRaw("Vertical");
 
             rb.velocity = new Vector2(move.x * playerSpeed, move.y * playerSpeed);
+
+            if(rb.velocity != Vector2.zero)
+            {
+                pv.moving = true;
+            }
+            else
+            {
+                pv.moving = false;
+            }
 
             //Credit robertbu on Unity Answers for mouse look
             Vector3 pos = c.WorldToScreenPoint(transform.position);
@@ -81,7 +149,83 @@ public class PlayerMovement : MonoBehaviour
                 if (gm.gs.canShoot) //Shoot gun
                     gm.gs.shoot();
         }
+        else if(controller)
+        {
+            touchUI.SetActive(false);
+            otherTouchUI.SetActive(false);
+
+            if (Input.GetButton("Jump"))
+            {
+                gm.gs.reload();
+                colorReloads = true;
+            }
+
+            if (Input.GetButton("Debug Previous"))
+            {
+                Debug.Log("Controller Toss");
+                gm.tossGun();
+            }
+
+            if (Input.GetButton("Debug Next"))
+            {
+                Debug.Log("Controller Swap");
+                gm.swapGun();
+            }
+
+            if (colorReloads)
+            {
+                if (Input.GetButton("Fire3"))
+                {
+                    gm.gs.reloadRed();
+                    colorReloads = false;
+                }
+                if (Input.GetButton("Fire2"))
+                {
+                    gm.gs.reloadBlue();
+                    colorReloads = false;
+                }
+                if (Input.GetButton("Fire1"))
+                {
+                    gm.gs.reloadPink();
+                    colorReloads = false;
+                }
+            }
+
+            //Debug.Log("c");
+            move.x = Input.GetAxisRaw("Horizontal");
+            move.y = Input.GetAxisRaw("Vertical");
+
+            rb.velocity = new Vector2(move.x * playerSpeed, move.y * playerSpeed);
+
+            if (rb.velocity != Vector2.zero)
+            {
+                pv.moving = true;
+            }
+            else
+            {
+                pv.moving = false;
+            }
+
+            rightstick = new Vector2(Input.GetAxis("R_Horizontal"), Input.GetAxis("R_Vertical"));
+
+            if (rightstick.magnitude >= 1f)
+            {
+                //Debug.Log(rightstick.magnitude);
+                Vector3 currRotation = Vector3.left * rightstick.x + Vector3.up * rightstick.y;
+                Quaternion playerRotation = Quaternion.LookRotation(currRotation, Vector3.forward);
+
+                rb.SetRotation(playerRotation);
+
+                if (gm.gs.canShoot) //Shoot gun
+                    gm.gs.shoot();
+            }
+        }
     }
+
+    //private void LateUpdate()
+    //{
+    //    oldpos = rb.position;
+    //}
 
     private void OnCollisionEnter2D(Collision2D collision) //Use this for player entering hazards
     {
